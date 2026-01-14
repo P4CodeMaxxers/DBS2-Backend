f306d7f35c0d4277179649da9fcb27777261516b
maya-dn <trsignals@gmail.com>
2026-01-14 14:30:10 -0800

    linking

--- __init__.py
+++ __init__.py
@@ -39,6 +39,7 @@ cors = CORS(
        'http://127.0.0.1:4000',
        'https://open-coding-society.github.io',
        'https://pages.opencodingsociety.com',
+       'https://p4codemaxxers.github.io'
    ],
    methods=["GET", "POST", "PUT", "OPTIONS"]
 )
